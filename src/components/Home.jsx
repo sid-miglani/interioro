5e47d0653ec30a415ab42ae4c1f851445abf7248
Siddharth Miglani <sidmig001@gmail.com>
2024-06-06 15:06:22 +0530

    Chnages in link in home componenet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,21 +23,21 @@ const Home = () => {
  </div>
   <div className='flex'>
     <div className='relative'>
-      <img className='w-[450px] h-[450px] ml-10' src="./src/images/room-house-decorated-with-brazilian-folklore-design.jpg"></img>
+      <img className='w-[450px] h-[450px] ml-10' src="../images/room-house-decorated-with-brazilian-folklore-design.jpg"></img>
       <div className='absolute top-60 left-32 text-white'>
         <h1 className=' font-bold text-3xl mb-6 text-gray-700'>LIVING</h1>
         <a href='/products' className='bg-white text-black py-2 px-6 mx-6 hover:opacity-85 text-lg'>VIEW PRODUCTS</a>
       </div>
     </div>
     <div className='relative'>
-      <img className='w-[450px] h-[450px] ml-10' src="./src/images/3d-rendering-loft-luxury-living-room-with-bookshelf-near-bookshelf.jpg"></img>
+      <img className='w-[450px] h-[450px] ml-10' src="../images/3d-rendering-loft-luxury-living-room-with-bookshelf-near-bookshelf.jpg"></img>
       <div className='absolute top-60 left-32 text-white'>
       <h1 className='font-bold text-3xl mb-6 text-gray-700'>DINING</h1>
       <a href='/products' className='bg-white text-black py-2 px-6 mx-6 hover:opacity-85 text-lg'>VIEW PRODUCTS</a>
       </div>
     </div>
     <div className='relative top-0'>
-      <img className='w-[450px] h-[450px] ml-11' src='./src/images/luxury-bedroom-suite-resort-high-rise-hotel-with-working-table.jpg'></img>
+      <img className='w-[450px] h-[450px] ml-11' src='../images/luxury-bedroom-suite-resort-high-rise-hotel-with-working-table.jpg'></img>
       <div className='absolute top-60 left-32 text-white '>
       <h1 className='font-bold text-3xl mb-6 text-gray-700'>BEDROOM</h1>
       <a href='/products' className='bg-white text-black py-2 px-6 mx-6 hover:opacity-85 text-lg'>VIEW PRODUCTS</a>
@@ -55,7 +55,7 @@ const Home = () => {
         <a href='/contact' className='py-2 px-8 ml-20 bg-black text-white text-xl border hover:opacity-75'>CONTACT US</a>
     </div>
     <div className='mr-8 w-[2000px]'>
-    <video src='./src/images/homevideo.mp4' typeof='mp4' autoPlay muted loop></video>
+    <video src='../images/homevideo.mp4' typeof='mp4' autoPlay muted loop></video>
     </div>
   </div>
   <div className='text-center my-10'>
@@ -67,17 +67,17 @@ const Home = () => {
     </h1>
   </div>
   <div className='flex mx-10 my-10'>
-    <div><img className='w-[330px] h-[330px] mr-8' src="./src/images/interior-design-with-photoframes-plants.jpg" alt="des-img" />
+    <div><img className='w-[330px] h-[330px] mr-8' src="../images/interior-design-with-photoframes-plants.jpg" alt="des-img" />
     <h1 className='tracking-wider text-gray-400 ml-4'>WILLOW SINGLE SEATER SOFA</h1><h1 className='text-gray-400 ml-3'>$600</h1></div>
-    <div><img  className='w-[330px] h-[330px] mr-8' src="./src/images/still-life-shoe-rack-indoors.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>SIDE TABLE</h1><h1 className='text-gray-400 ml-3'>$900</h1></div>
-    <div><img  className='w-[330px] h-[330px] mr-8' src="./src/images/room-interior-design.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>DINING TABLE</h1><h1 className='text-gray-400 ml-3'>$750</h1></div>
-    <div><img  className='w-[330px] h-[330px]' src="./src/images/realistic-interior-design-with-furniture.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>CHARLOTTE SOFA</h1><h1 className='text-gray-400 ml-3'>$1300</h1></div>
+    <div><img  className='w-[330px] h-[330px] mr-8' src="../images/still-life-shoe-rack-indoors.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>SIDE TABLE</h1><h1 className='text-gray-400 ml-3'>$900</h1></div>
+    <div><img  className='w-[330px] h-[330px] mr-8' src="../images/room-interior-design.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>DINING TABLE</h1><h1 className='text-gray-400 ml-3'>$750</h1></div>
+    <div><img  className='w-[330px] h-[330px]' src="../images/realistic-interior-design-with-furniture.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>CHARLOTTE SOFA</h1><h1 className='text-gray-400 ml-3'>$1300</h1></div>
   </div>
   <div className='flex mx-10 my-10'>
-    <div><img className='w-[330px] h-[330px] mr-8' src="./src/images/beautiful-girl-standing-room-looking-mirror.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>BETHANIE FLOOR MIRROR</h1><h1 className='text-gray-400 ml-3'>$1299</h1></div>
-    <div><img className='w-[330px] h-[330px] mr-8' src="./src/images/coffee-cup-table.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>COFFEE TABLE</h1><h1 className='text-gray-400 ml-3'>$850</h1></div>
-    <div><img className='w-[330px] h-[330px] mr-8' src="./src/images/bangkok-thailand-august-12-2016-beautiful-luxury-living-room.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>LIGHTNING DESIGN</h1><h1 className='text-gray-400 ml-3'>$1199</h1></div>
-    <div><img className='w-[330px] h-[330px] mr-8' src="./src/images/velvet-interior-classic-cushion-relax.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>OMAHA SVIWEL CHAIR</h1><h1 className='text-gray-400 ml-3'>$1099</h1></div>
+    <div><img className='w-[330px] h-[330px] mr-8' src="../images/beautiful-girl-standing-room-looking-mirror.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>BETHANIE FLOOR MIRROR</h1><h1 className='text-gray-400 ml-3'>$1299</h1></div>
+    <div><img className='w-[330px] h-[330px] mr-8' src="../images/coffee-cup-table.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>COFFEE TABLE</h1><h1 className='text-gray-400 ml-3'>$850</h1></div>
+    <div><img className='w-[330px] h-[330px] mr-8' src="../images/bangkok-thailand-august-12-2016-beautiful-luxury-living-room.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>LIGHTNING DESIGN</h1><h1 className='text-gray-400 ml-3'>$1199</h1></div>
+    <div><img className='w-[330px] h-[330px] mr-8' src="../images/velvet-interior-classic-cushion-relax.jpg" alt="des-img" /> <h1 className='tracking-wider text-gray-400 ml-4'>OMAHA SVIWEL CHAIR</h1><h1 className='text-gray-400 ml-3'>$1099</h1></div>
   </div>
   <div className='text-center'>
     <h1 className='tracking-wider text-3xl text-gray-500 mb-4'>
